ce6140ebfdee2d85d8f624b793a4ab54e3f3e9b1
OrkVasya <OrkVasya@gmail.com>
2023-02-19 23:15:40 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println(surnames.length);
         float[] prices = new float[40];
         System.out.println(prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Sokol";
+        names[1] = "Pokol";
+        names[2] = "Rokol";
+        names[3] = "Mokol";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
